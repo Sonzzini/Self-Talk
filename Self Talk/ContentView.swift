6b6374f8398175c34e21ebab63f704365bb9db29
Sonzzini <paulo.sonzzini@gmail.com>
2024-04-09 08:30:59 -0300

    Initial Commit

---
+++ Self Talk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Self Talk
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Self Talk/Self_TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Self_TalkApp.swift
+//  Self Talk
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Self_TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
